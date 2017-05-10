2b25cb07e45d9e8ce69737830f39a67aa8f9df86
unDocUMeantIt <m@reaktanz.de>
2017-05-10 16:36:31 +0200

    updated docs (esp. vignette)
      - elaborated a bit on new output formats and language support

--- R/sylly-package.R
+++ R/sylly-package.R
@@ -4,7 +4,7 @@
 #' Package: \tab sylly\cr
 #' Type: \tab Package\cr
 #' Version: \tab 0.1-1\cr
-#' Date: \tab 2017-05-08\cr
+#' Date: \tab 2017-05-10\cr
 #' Depends: \tab R (>= 2.10.0),methods\cr
 #' Encoding: \tab UTF-8\cr
 #' License: \tab GPL (>= 3)\cr
